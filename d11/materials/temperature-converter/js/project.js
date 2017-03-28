5489f3a41d203e88354776cc43c033d3043a34ee
fadujr <swingy33@yahoo.com>
2017-03-28 18:38:48 -0400

    Temperature Converter

--- d11/materials/temperature-converter/js/project.js
+++ d11/materials/temperature-converter/js/project.js
@@ -1,7 +1,17 @@
+
 $("button").on("click", function()
 {
-	var c = $("input").val();
-	var f = 1.8 * parseInt(c) + 32;
-
-	$("span").text(f.toString());
+	if ($("select").val("deg-f"))
+	{	
+		var c = $("input").val();
+		var f = 1.8 * parseInt(c) + 32;
+		$("span").text(f.toString());
+	}
+	if ($("select").val("deg-c"))
+	{
+		var f = $("input").val();
+		var c = 0.5555 * (parseInt(f) - 32);
+		$("span").text(c.toString());
+		$("p").text("That is equal to degrees Celsius");
+	}
 });
